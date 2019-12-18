1180695ad8cc0c45f8018ad1147c534e2e7455a1
collins <ugwucollins64@gmail.com>
2019-12-18 16:10:04 +0100

    fix: comfirmable removed from user model to allow authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+         :recoverable, :rememberable, :validatable
+         
   validates :first_name, presence: true
   validates :last_name, presence: true
 
