65e2113bc7cde7c226c6883d00087b0e0b94d654
collins <ugwucollins64@gmail.com>
2019-12-20 19:59:12 +0100

    test users and post

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_path
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get posts_path
+  #   assert_response :success
+  # end
 
   # test "should get show" do
   #   get '/posts/:id'
