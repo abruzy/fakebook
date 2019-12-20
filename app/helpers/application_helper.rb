9f88772e3810cde0ef148a7817ed8efd160760b4
collins <ugwucollins64@gmail.com>
2019-12-20 02:44:32 +0100

    restructure the view and add some helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def gravatar_for(user, size = 30)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravater_url, alt: user.first_name, class: "rounded-circle") 
+    image_tag(gravater_url, alt: user.first_name, class: 'rounded-circle')
   end
 
   def name
     current_user.first_name + ' ' + current_user.last_name
   end
+
+  def post_owner(post)
+    post.user.first_name + ' ' + post.user.last_name
+  end
+
 end
