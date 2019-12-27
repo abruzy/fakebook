79f858ef5755d50ffa5bb34475214ac12c86c706
collins <ugwucollins64@gmail.com>
2019-12-27 13:45:35 +0100

    update nokogiri gem

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    description { Faker::Lorem::paragraph }
+    description { Faker::Lorem.paragraphs }
     image { 'http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802' }
     association :user, factory: :user
   end 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post do
-  let(:post) { FactoryBot.create(:post) }
+  let(:post) { FactoryBot.build(:post) }
 
-  # describe "validations" do
-  #   it { is_expected.to validate_presence_of(:description) }
-  #   it { should ensure_length_of(:description).is_at_least(5) }
-  # end
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:description) }
+    it { should ensure_length_of(:description).is_at_least(5) }
+  end
 
   describe "association" do
     it { belong_to(:user) }
   end
 
   it 'is valid post if it has at least five character length content' do
-    expect(post).to be_valid
+    expect(Post.new).to be_valid
   end
 
   it 'is invalid post if it does not have content' do
