a49590dc8029c397c19d0abe391ba10a838cd1b8
collins <ugwucollins64@gmail.com>
2019-12-28 17:36:40 +0100

    post faker modified to accomodate column

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :post do
     description { Faker::Lorem::paragraph }
-    image { "#{Rails.root}/spec/fixtures/avatar.png" }
     association :user, factory: :user
   end 
 end
