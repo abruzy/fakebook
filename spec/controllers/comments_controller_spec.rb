2a096b35b8db5f6e6a981dfa1a67a51dcdb3edb1
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-01-01 23:55:36 +0100

    comment controller test added

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,4 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe 'create new comment' do
+    context 'when comment creation succeeds' do
+      it 'creates comment with body' do
+        user = FactoryBot.create(:user)
+        post = user.posts.create(description: 'This is the comment')
+        params = {
+          body: 'This is the comment',
+          user_id: user.id
+        }
+        expect do
+          post.comments.create(params)
+        end.to change(Comment, :count).by(1)
+      end
+    end
+
+    context 'when comment creation fails' do
+      it 'should not create comment without body' do
+        user = FactoryBot.create(:user)
+        post = user.posts.create(description: 'This is the comment')
+        params = {
+          body: nil,
+          user_id: user.id
+        }
+        expect do
+          post.comments.create(params)
+        end.to_not change(Comment, :count)
+      end
+    end
+  end
 end
--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe FriendsController, type: :controller do
-end
+# RSpec.describe FriendsController, type: :controller do
+# end
