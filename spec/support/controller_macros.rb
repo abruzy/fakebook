e392741fcd967a5adbea9e1617d694152fac1fba
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-01-02 17:24:13 +0100

    linted

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,9 +1,11 @@
-module ControllerMacros    
+# frozen_string_literal: true
+
+module ControllerMacros
   def login_user
-      before(:each) do
-          @request.env["devise.mapping"] = Devise.mappings[:user]
-          user = FactoryBot.create(:user)
-          sign_in user
-      end
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
   end
 end
