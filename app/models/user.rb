707ed172fa23fc691b308fe1d42152074063da00
collins <ugwucollins64@gmail.com>
2020-01-09 14:12:27 +0100

    users can successfully sign up with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, 
-         :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: [:facebook]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -67,13 +67,23 @@ class User < ApplicationRecord
     User.all.map { |user| user unless friend?(user) || current_user == user }.compact
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
   def self.from_omniauth(auth)
+    password = Devise.friendly_token[0, 20]
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.first_name = auth.info.first_name  
-      user.last_name = auth.info.last_name        
-      # If you are using confirmable and the provider(s) you use validate emails, 
+      user.password = password
+      user.password_confirmation = password
+      user.first_name = 'Mr '
+      user.last_name = auth.info.name
+      # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
     end
