9d41375bf832143091acaa82f4ad31223981c815
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-01-10 16:41:10 +0100

    linted

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -14,11 +14,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :feature) do
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    unless driver_shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :truncation
-    end
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
